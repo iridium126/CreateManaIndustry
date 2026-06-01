0953fb7621d1a86a6131361bce6c8be0cbcb8c5c
iridium126 <iridium192@126.com>
2026-06-01 17:48:17 +0800

    Add general PartialModel replacement support when flywheel is off

---
+++ src/main/java/com/iridium126/createtricks/content/kinetics/TemporaryStressRenderContext.java
@@ -0,0 +1,30 @@
+package com.iridium126.createtricks.content.kinetics;
+
+import dev.engine_room.flywheel.lib.model.baked.PartialModel;
+import net.minecraft.world.level.block.entity.BlockEntity;
+
+public final class TemporaryStressRenderContext {
+	private static final ThreadLocal<BlockEntity> RENDERED_BLOCK_ENTITY = new ThreadLocal<>();
+
+	private TemporaryStressRenderContext() {}
+
+	public static void set(BlockEntity be) {
+		RENDERED_BLOCK_ENTITY.set(be);
+	}
+
+	public static void clear(BlockEntity be) {
+		if (RENDERED_BLOCK_ENTITY.get() == be)
+			RENDERED_BLOCK_ENTITY.remove();
+	}
+
+	public static BlockEntity get() {
+		return RENDERED_BLOCK_ENTITY.get();
+	}
+
+	public static PartialModel replace(PartialModel partial) {
+		BlockEntity be = RENDERED_BLOCK_ENTITY.get();
+		if (be == null)
+			return partial;
+		return TemporaryStressModel.replacementOrSelf(be, partial);
+	}
+}
