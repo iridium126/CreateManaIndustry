09894c28a1cd5b72e5b02188bfbe4acf7718f5a0
iridium126 <iridium192@126.com>
2026-06-02 17:24:28 +0800

    Update textures and remove useless code

--- src/main/java/com/iridium126/createtricks/content/kinetics/TemporaryStressVisual.java
+++
@@ -1,8 +0,0 @@
-package com.iridium126.createtricks.content.kinetics;
-
-import dev.engine_room.flywheel.lib.model.baked.PartialModel;
-import net.minecraft.core.Direction;
-
-public interface TemporaryStressVisual {
-	void createtricks$setTemporaryStressModel(PartialModel model, Direction sourceDirection);
-}
